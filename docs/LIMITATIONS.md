1c453e0e132e599503bda1608cd9a57688c49400
satyrnwastaken <satyrn@disroot.org>
2022-03-18 04:34:00 +0000

    the great variable overhaul

--- docs/LIMITATIONS.md
+++ docs/LIMITATIONS.md
@@ -1,9 +1,45 @@
 # limitations
-these are limitations that nothing will be done about in the future.
-
-they will not be fixed due to being either impossible to fix, or requiring an unrealistic amount of work.
+these are the current limitations of void. these may be fixed in the future.
 
 limitations:
 - comments cannot be on the same line as a piece of code
-	* example 1: `do_stuff() // this comment shouldn't be here!` <- improper
-	* example 2: `// this comment can be here!` <- proper
+	* (bad) example 1:
+        + ```
+        do_stuff() // example comment
+        ```
+	* (good) example 2:
+        + ```
+        // example comment
+        ```
+- dictionary keys/values cannot contain a colon
+	* (bad) example 1:
+        + ```
+        dict = {
+                "key": "value :"
+        }
+        ```
+	* (good) example 2:
+        + ```
+        dict = {
+                "key": "value"
+        }
+        ```
+- dictionary definitions cannot contain key/value pairs where the value is also a dictionary
+    * note: you can (in the future) define a mutable dictionary with the key and an empty value, and then change the value to the sub-dictionary.
+    * (bad) example 1:
+        + ```
+        dict = {
+                "testkey1": {
+                    "testkey2": "testvalue1"
+                }
+        }
+        ```
+    * (good) example 2:
+        + ```
+        dict = {
+                "testkey1": ""
+        }
+        dict["testkey1"] = {
+                "testkey2": "testvalue1"
+        }
+        ```
--- interpreter.py
+++ interpreter.py
@@ -54,35 +54,37 @@ def get_data(line_num, break_char, ccmd):
         del data[-1]
     return data
 
-def parse_input(unparsed_input, scope, **kw):
-    parsed_input = unparsed_input
+def parse_string(unparsed_string, inter_var, **kw):
+    parsed_string = unparsed_string
     
-    if re.match('f\".*\"', parsed_input) or re.match("f\'.*\'", parsed_input):
-        parsed_input = list(parsed_input)
-        del parsed_input[0]
-        parsed_input = "".join(parsed_input)
-        fstr_var = re.findall("{.*}", parsed_input)
-        if scope != "root":
-            parsing_var = { **var["global"], **var["local"][scope] }
-        else:
-            parsing_var = var["global"]
+    if re.match('f\".*\"', parsed_string) or re.match("f\'.*\'", parsed_string):
+        parsed_string = list(parsed_string)
+        del parsed_string[0]
+        parsed_string = "".join(parsed_string)
+        fstr_var = re.findall("{.*}", parsed_string)
         for fstr in fstr_var:
-            val = parsing_var[fstr.replace("{", "").replace("}", "")]["val"]
-            parsed_input = parsed_input.replace(fstr, val)     
-    elif re.match('\".*\"', parsed_input) or re.match("\'.*\'", parsed_input):
-        parsed_input = list(parsed_input)
-        del parsed_input[0]
-        del parsed_input[-1]
-        parsed_input = "".join(parsed_input)
+            val = inter_var[fstr.replace("{", "", 1).replace("}", "", 1)]["val"]
+            parsed_string = parsed_string.replace(fstr, val)     
+    elif re.match('\".*\"', parsed_string) or re.match("\'.*\'", parsed_string):
+        parsed_string = list(parsed_string)
+        del parsed_string[0]
+        del parsed_string[-1]
+        parsed_string = "".join(parsed_string)
     
-    return parsed_input
+    return parsed_string
 
-def parse_list(unparsed_list, scope, **kw):
+def parse_list(unparsed_list, inter_var, scope, ccmd, **kw):
     if unparsed_list == "()" or unparsed_list == "[]":
         return ()
-    
+
     parsed_list = unparsed_list
-    
+
+    if "," not in parsed_list:
+        parsed_list = list(parsed_list)
+        del parsed_list[0]
+        del parsed_list[-1]
+        return inter_inline_expr("".join(parsed_list), inter_var, scope, ccmd)
+        
     if parsed_list[0] == "[" and parsed_list[-1] == "]":
         parsed_list = list(parsed_list)
         parsed_list[0] = "("
@@ -119,7 +121,7 @@ def parse_list(unparsed_list, scope, **kw):
                 val = parsing_var[fstring_var.replace("{", "").replace("}", "")]["val"]
                 parsed_list = parsed_list.replace(fstring, fstring.replace(fstring_var, val).replace("f", "", 1))
         for num, string in enumerate(scopes[scope]["temp"]["arg_strs"]):
-            parsed_list = parsed_list.replace(f"'str{num}',", string)
+            parsed_list = parsed_list.replace(f'"str{num}",', string)
 
     else:
         root["temp"]["arg_fstrs"] = [ *re.findall('f\".*\"', parsed_list), *re.findall("f\'.*\'", parsed_list) ]
@@ -129,41 +131,33 @@ def parse_list(unparsed_list, scope, **kw):
                 val = parsing_var[fstring_var.replace("{", "").replace("}", "")]["val"]
                 parsed_list = parsed_list.replace(fstring, fstring.replace(fstring_var, val).replace("f", "", 1))
         for num, string in enumerate(root["temp"]["arg_strs"]):
-            parsed_list = parsed_list.replace(f"'str{num}',", string)
+            parsed_list = parsed_list.replace(f'"str{num}",', string)
     
     parsed_list = literal_eval(parsed_list)
 
     return parsed_list
 
-def init_var(line_num, var_type, ccmd, scope):
+def init_var(line_num, var_type, ccmd, inter_var, scope):
     var_name = re.sub("=.*", "", ccmd.replace(f"{var_type} ", "")).strip()
-    var_val = parse_input(re.sub(f"{var_type} {var_name}.*=", "", ccmd).lstrip(), scope)
-
-    if var_val[0] == "[":
-        var_val = get_data(line_num, "]", ccmd)
-
-    elif var_val[0] == "{":
-        data = get_data(line_num, "}", ccmd)
-        var_val = {}
-        for pair in data:
-            split_pair = pair.split(": ")
-            var_val[split_pair[0]] = split_pair[1]
+    var_val = inter_inline_expr(re.sub(f"{var_type} {var_name}.*=", "", ccmd).lstrip(), inter_var, scope, ccmd)
 
     if scope == "root":
         var["global"][var_name] = {
             "type": var_type,
+            "scope": "root",
             "val": var_val
         }
     else:
         var["local"][scope][var_name] = {
             "type": var_type,
+            "scope": scope,
             "val": var_val
         }
 
-def interpret_func(ccmd, scope):
+def interpret_func(ccmd, inter_var, scope):
     full_func = re.findall("[^\s]+\(.*\)", ccmd)[0]
     func_name = full_func.replace("(", "")
-    args = parse_list(re.findall("\(.*\)", full_func)[0], scope)
+    args = parse_list(re.findall("\(.*\)", full_func)[0], inter_var, scope, ccmd)
     func_name = ccmd.replace(re.findall("\(.*\)", full_func)[0], "")
     if funcs[func_name]["type"] == "included":
         if isinstance(args, tuple):
@@ -173,16 +167,30 @@ def interpret_func(ccmd, scope):
     else:
         return interpret(funcs[func_name]["code"], args)
 
-def modify_var(line_num, scmd, global_var, scope_var, scope):
-    global var
-    operator = scmd[1][0]
-    if operator != "=":
-        operator
-    else:     
-        if scmd[0] in scope_var:
-            var["local"][scope][scmd[0]] = scmd[2]
+def modify_var(line_num, scmd, inter_var, scope):
+    operator = scmd[1][:-1]
+    if operator:
+        if scmd[0] in var["local"][scope]:
+            if var["local"][scope][scmd[0]]["type"] == "var":
+                var["local"][scope][scmd[0]]["val"] = ops[operator](var["local"][scope][scmd[0]]["val"], int(scmd[2]))
+            else:
+                void_error("const_immutable", arg_line_num=line_num)
         else:
-            var["global"][scmd[0]] = scmd[2]
+            if var["global"][scmd[0]]["type"] == "var":
+                var["global"][scmd[0]]["val"] = ops[operator](var["global"][scmd[0]]["val"], int(scmd[2]))
+            else:
+                void_error("const_immutable", arg_line_num=line_num)
+    else:
+        if scmd[0] in var["local"][scope]:
+            if var["local"][scope][scmd[0]]["type"] == "var":
+                var["local"][scope][scmd[0]]["val"] = scmd[2]
+            else:
+                void_error("const_immutable", arg_line_num=line_num)
+        else:
+            if var["global"][scmd[0]]["type"] == "var":
+                var["global"][scmd[0]]["val"] = scmd[2]
+            else:
+                void_error("const_immutable", arg_line_num=line_num)
 
 def handle_op(line_num, op, ccmd, scmd, global_var, scope_var):
     final_op = scmd
@@ -201,19 +209,56 @@ def exit_void(*args):
         if len(args) > 0:
             print("\n")
         runtime = time() * 1000 - runtime
-        print(f"time to setup: {round(setuptime, 4)}ms")
-        print(f"time to run: {round(runtime, 4)}ms")
+        print(f"time to setup: {round(setuptime, 5)}ms")
+        print(f"time to run: {round(runtime, 5)}ms")
     _exit(0)
 
-def void_error(err):
-    print("ERROR |", errors[err])
+def void_error(err, **kw):
+    if err in no_line_errors:
+        print("ERROR |", no_line_errors[err])
+    else:
+        print(f"ERROR (line {kw.get('arg_line_num')+file_offset}) |", errors[err])
     _exit(1)
 
-def inter_inline_expr(expr):
-	try:
-		return int(expr)
-	except:
-		print("uhhhhhh")
+def get_var(scope):
+    if scope != "root":
+        merged_var = var["global"]
+        scope_var = var["local"][scope]
+        for svar in scope_var:
+            merged_var[svar] = scope_var[svar]["val"]
+        return merged_var
+    else:
+        return var["global"]
+
+def inter_inline_expr(expr, inter_var, scope, ccmd):
+    try:
+        return int(expr)
+    except:
+        try:
+            return float(expr)
+        except:
+            if expr in inter_var:
+                val = inter_var[expr]["val"]
+                try:
+                    return int(val)
+                except:
+                    try:
+                        return float(val)
+                    except:
+                        return val
+            elif expr[0] == "[":
+                return get_data(line_num, "]", ccmd)
+            elif expr[0] == "{":
+                data = get_data(line_num, "}", ccmd)
+                final_dict = {}
+                for pair in data:
+                    split_pair = pair.split(": ")
+                    final_dict[split_pair[0]] = split_pair[1]
+                return final_dict
+            elif re.match('\".*\"', expr) or re.match("\'.*\'", expr):
+                return parse_string(expr, scope)
+            else:
+                interpret_func(expr, inter_var, scope)
 
 # interpret
 def interpret(data, args, **kw):
@@ -223,21 +268,15 @@ def interpret(data, args, **kw):
         ccmd = line.lstrip()
         scmd = ccmd.split()
         scope = get_scope(line_num, ccmd)
-        global_var = var["global"]
-        if scope != "root":
-            scope_var = var["local"][scope]
-        else:
-            scope_var = global_var
-        if scmd in ops:
-            returned = handle_op(line_num, scmd[1], ccmd, scmd, global_var, scope_var)
-        elif scmd[0] == "var":
-            returned = init_var(line_num, "var", ccmd, scope)
+        inter_var = get_var(scope)
+        if scmd[0] == "var":
+            returned = init_var(line_num, "var", ccmd, inter_var, scope) 
         elif scmd[0] == "const":
-            returned = init_var(line_num, "const", ccmd, scope)
+            returned = init_var(line_num, "const", ccmd, inter_var, scope)
         elif ccmd == "break" and "scope" in kw:
             returned = f"{kw.get('scope')}_break"
         elif len(scmd) > 1 and scmd[1][-1] == "=":
-            returned = modify_var(line_num, scmd, global_var, scope_var, scope)
+            returned = modify_var(line_num, scmd, inter_var, scope)
         elif ccmd == "forever {":
             forever_data = get_data(line_num, "}", ccmd)
             while void_running:
@@ -249,7 +288,7 @@ def interpret(data, args, **kw):
             num_expr = scmd
             del num_expr[0]
             del num_expr[-1]
-            repeat_times = inter_inline_expr("".join(num_expr))
+            repeat_times = inter_inline_expr("".join(num_expr), inter_var, scope, ccmd)
             repeated_i = 1
             while repeated_i < repeat_times:
                 breakb = interpret(repeat_data, args, scope=scope)
@@ -257,7 +296,7 @@ def interpret(data, args, **kw):
                     return
                 repeated_i += 1
         elif ccmd[-1] == ")" and re.findall("[^\s]+\(.*\)", ccmd)[0] == ccmd:
-            returned = interpret_func(ccmd, scope)
+            returned = interpret_func(ccmd, inter_var, scope)
         if returned:
             return returned
 
@@ -273,10 +312,14 @@ scopes = {}
 runtime = 0
 setuptime = 0
 void_running = False
-errors = {
+no_line_errors = {
     "file_not_specified": "a file must be specified with the -f flag.",
     "file_not_found": "the specified file cannot be found.",
     "file_not_void": "the specified file must have the .void file extension.",
+}
+errors = {
+    "cannot_concat_int": "integers and strings cannot be concatenated.",
+    "const_immutable": "variables defined with const are immutable; they cannot be modified."
 }
 root = {
     "temp": {}
@@ -285,7 +328,21 @@ var = {
     "global": {},
     "local": {},
 }
-ops = ("+", "-", "/", "*", "@", "**", "%")
+ops = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv,
+    "%": operator.mod,
+    "//": operator.floordiv,
+    "**": operator.pow,
+    "&": operator.and_,
+    "|": operator.or_,
+    "^": operator.xor,
+    ">>": operator.rshift,
+    "<<": operator.lshift,
+}
+file_offset = 0
 
 # process args
 a = []
@@ -335,6 +392,7 @@ for line_num, line in enumerate(file):
 
 # clean up step 2
 while "" in file:
+    file_offset += 1
     file.remove("")
 
 # process funcs and scopes
