c698480cc0e7c1a7b2eac2d43ab5eddb3144c3ba
satyrnwastaken <satyrn@disroot.org>
2022-03-29 14:36:13 +0000

    fix actions

--- scripts/actions.sh
+++ scripts/actions.sh
@@ -2,5 +2,10 @@
 
 commit=$(git log -1 | head -n 1 | sed -e "s/commit//")
 
-echo -e "building...\n"
+echo -e "building void...\n"
+cd void
+go build -ldflags "-X 'utils.Commit=$commit'" -v
+
+echo -e "\nbuilding vpkg...\n"
+cd ../vpkg
 go build -ldflags "-X 'utils.Commit=$commit'" -v
